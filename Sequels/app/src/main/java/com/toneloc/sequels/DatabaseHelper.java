9c07fcf2feffe80d2934629ca39aa3d8de804358
toneloc <awklausing@gmail.com>
2016-06-22 11:31:01 -0500

    more changes

--- Sequels/app/src/main/java/com/toneloc/sequels/DatabaseHelper.java
+++ Sequels/app/src/main/java/com/toneloc/sequels/DatabaseHelper.java
@@ -12,6 +12,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final int DB_VERSION = 1; //change as we upgrade
     public static final String DB_NAME = "ReallyBadSequels.db"; //name of your db
+    public static final String CREATE_TABLE = "CREATE TABLE sequels (id INT PRIMARY KEY, name TEXT);";
+    public static final String DROP_TABLE = "DROP TABLE IF EXISTS sequels;";
 
 
     public DatabaseHelper(Context context) {
@@ -21,13 +23,21 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(DROP_TABLE);
+        onCreate(db);
+    }
+
+    //NOT an override
+    public void seedDatabase() {
 
     }
 
-
+    public void insertSequel(int id, String name) {
+        public static final String = "INSERT INTO  .. "
+    }
 }
